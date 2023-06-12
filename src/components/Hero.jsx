55d9fe41807ed4fe1c07a2858a3d0f491e8e814e
SpoonMouse <tracyd153@gmail.com>
2023-06-11 21:39:07 -0500

    added navbar component along with premade assets :)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import Computers from './canvas/Computers'; 
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mxauto flex flex-row items-start gap-5`}>
+          <div className='flex flex-col justify-center items-center mt-5'></div>
+      </div>
+    </section>
   )
 }
 
