beb955b4f04161fdc301d402bd2acaa26eed3e0f
SpoonMouse <tracyd153@gmail.com>
2023-06-22 13:44:40 -0500

    deleted some sections

--- src/components/index.js
+++ src/components/index.js
@@ -3,17 +3,15 @@ import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
 import Tech from './Tech';
-import Experience from './Experience';
-import Works from './Works';
 import Feedbacks from './Feedbacks';
 import Contact from './Contact';
+import Works from './Works';
 
 export {
   Hero,
   Navbar,
   About,
   Tech,
-  Experience,
   Works,
   Feedbacks,
   Contact,
